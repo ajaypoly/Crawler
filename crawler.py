23bf41fec4cb9ed959201f7fab0222b0963fbf5f
AJAY POLY <ajay@hamon.in>
2022-03-18 11:45:14 +0530

    add function for get_songs and lyrics

--- crawler.py
+++ crawler.py
@@ -14,11 +14,38 @@ def get_artist(url):
     return ret
 
 
+def get_songs(artist_url):
+    songs = []
+    r = requests.get(artist_url)
+    body = r.content
+    soup = BeautifulSoup(body, features="html.parser")
+    tracklists = soup.find("table", {"class": "tracklist"})
+    links = tracklists.find_all("a")
+    for i in links:
+        songs.append((i.text, i['href']))
+    return songs
+
+
+def get_lyrics(song_url):
+    r = requests.get(song_url)
+    soup = BeautifulSoup(r.content, features="html.parser")
+    lyrics_div = soup.find("p", {"id": "songLyricsDiv"})
+    lyrics = lyrics_div.text
+    return lyrics
+
+
 def crawl():
     artists = get_artist("https://www.songlyrics.com/a/")
     for name, link in artists:
         print(name, "   :   ", link)
-   
+        songs = get_songs(link)
+        for song, song_link in songs:
+            with open("lyric.txt", "a") as d:
+                lyrics = get_lyrics(song_link)
+                d.write("\n\n\n\n")
+                d.write(song)
+                d.write("\n\n\n\n")
+                d.write(lyrics)
 
 
 if __name__ == "__main__":
